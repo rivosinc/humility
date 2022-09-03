f1a078f9cbada8c976f18d5801e95a04d09b13b4
Andrew Zoghby <azoghby@rivosinc.com>
2022-09-02 17:17:54 -0700

    cmd: halt: add `humility halt` to stop the core

---
+++ cmd/halt/src/lib.rs
@@ -0,0 +1,52 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+//! ## `humility halt`
+//!
+//! `humility halt` will halt the system using the debug pin
+//!
+
+use anyhow::Result;
+use clap::Command as ClapCommand;
+use clap::{CommandFactory, Parser};
+use humility::core::Core;
+use humility::hubris::*;
+use humility_cmd::{Archive, Attach, Command, Run, Validate};
+
+#[derive(Parser, Debug)]
+#[clap(name = "halt", about = env!("CARGO_PKG_DESCRIPTION"))]
+struct HaltArgs {}
+
+fn halt(
+    _hubris: &HubrisArchive,
+    core: &mut dyn Core,
+    _subargs: &[String],
+) -> Result<()> {
+    let r = core.halt();
+
+    if r.is_err() {
+        humility::msg!(
+            "There was an error when halting. \
+            The chip may be in an unknown state!"
+        );
+        humility::msg!("Full error: {:x?}", r);
+    } else {
+        humility::msg!("core halted");
+    }
+
+    Ok(())
+}
+
+pub fn init() -> (Command, ClapCommand<'static>) {
+    (
+        Command::Attached {
+            name: "halt",
+            archive: Archive::Required,
+            attach: Attach::LiveOnly,
+            validate: Validate::Match,
+            run: Run::Subargs(halt),
+        },
+        HaltArgs::command(),
+    )
+}
